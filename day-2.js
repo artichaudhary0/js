6383670d57a6f2a5184ca9d6f84ae077013d10eb
artichaudhary <chaudhary.aarti1998@gmail.com>
2024-07-12 09:15:19 +0530

    day 2

--- day-2.js
+++ day-2.js
@@ -1,21 +1,46 @@
-// Reverse a number using two pointer approach
+// // Reverse a number using two pointer approach
 
-function reverse_array(array) {
+// function reverse_array(array) {
     
-    left = 0 
-    right = array.length - 1 
-    while(left<right)
+//     left = 0 
+//     right = array.length - 1 
+//     while(left<right)
+//     {
+//         let temp = array[left]
+//         array[left] = array[right]
+//         array[right] = temp
+//         left++
+//         right--
+//     }
+//     return array
+// }
+
+// let result = reverse_array([54,23,345,234,432,436,43])
+
+// console.log(result)
+
+
+
+// // searching a pair which sum upto x(single pair)
+
+// function sum_pair()
+// {
+
+
+
+// }
+
+// 2-D array
+
+arr = [[1,2,3],[1,2,3],[1,2,3]]
+
+n = arr.length
+m = arr[0].length
+
+for(let i = 0 ; i<n ;i++)
     {
-        let temp = array[left]
-        array[left] = array[right]
-        array[right] = temp
-        left++
-        right--
+        for(let j = 0 ; j<m ;j++)
+            {
+                console.log(arr[i][j] + " ")
+            }
     }
-    return array
-}
-
-let result = reverse_array([54,23,345,234,432,436,43])
-
-console.log(result)
-
